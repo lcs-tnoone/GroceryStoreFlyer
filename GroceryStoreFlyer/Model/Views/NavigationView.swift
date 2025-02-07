8818dce1df4a647441d335e4a32ef9ca1cbaed85
Thomas Noone <tnoone@lcs.on.ca>
2025-02-06 19:30:19 -0500

    got the list view done

---
+++ GroceryStoreFlyer/Model/Views/NavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView.swift
+//  GroceryStoreFlyer
+//
+//  Created by Thomas Noone on 2025-02-06.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView()
+}
